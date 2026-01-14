facd8fa3f99f801eb2d8acf42590001eb8eea797
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-01-15 00:39:39 +0700

    refactor: move impl

--- packages/server/src/schemas/dictionary_term_bank_v3.rs
+++ packages/server/src/schemas/dictionary_term_bank_v3.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_valid::Validate;
-use std::{collections::HashMap, fs};
+use std::collections::HashMap;
 
 pub type DictionaryTermBankV3 = Vec<DictionaryTermBankV3Row>;
 
@@ -470,120 +470,7 @@ fn default_true() -> bool {
     true
 }
 
-impl DictionaryTermBankV3Row {
-    pub fn test(&self) {
-        let item = self.5.first().unwrap();
-        match item {
-            Term::Definition(_) => {
-                println!("DEBUG[1410]: this is String");
-            }
-            Term::DetailedDefinition(definition) => match definition.as_ref() {
-                DetailedDefinition::Text(_) => {
-                    println!("DEBUG[1410]: this is Text");
-                }
-                DetailedDefinition::Image(_) => {
-                    println!("DEBUG[1410]: this is Image");
-                }
-                DetailedDefinition::StructuredContent(sc) => {
-                    test_structured_content(sc.content.as_ref());
-                    // println!("DEBUG[1410]: this is StructuredContent");
-                }
-            },
-            Term::Deinflection(_) => {
-                println!("DEBUG[1410]: this is Array");
-            }
-        }
-    }
-}
-
-fn test_structured_content(content: &StructuredContent) {
-    match content {
-        StructuredContent::Text(_) => {
-            // println!("DEBUG[1410]: this is StructuredContentText");
-        }
-        StructuredContent::Array(arr) => {
-            // println!("DEBUG[1410]: this is StructuredContentArray");
-            for sc in arr.iter() {
-                test_structured_content(sc);
-            }
-        }
-        StructuredContent::Object(obj) => {
-            test_structured_content_object(obj);
-            // println!("DEBUG[1410]: this is StructuredContentObject");
-        }
-    }
-}
-
-fn test_structured_content_object(obj: &StructuredContentObject) {
-    match obj {
-        StructuredContentObject::Br(_) => {
-            println!("DEBUG[1410]: this is StructuredContentBr ");
-        }
-        StructuredContentObject::Ruby(_) => {
-            println!("DEBUG[1410]: this is StructuredContentRuby ");
-        }
-        StructuredContentObject::Rt(_) => {
-            println!("DEBUG[1410]: this is StructuredContentRt ");
-        }
-        StructuredContentObject::Rp(_) => {
-            println!("DEBUG[1410]: this is StructuredContentRp ");
-        }
-        StructuredContentObject::Table(_) => {
-            println!("DEBUG[1410]: this is StructuredContentTable ");
-        }
-        StructuredContentObject::Thead(_) => {
-            println!("DEBUG[1410]: this is StructuredContentThead ");
-        }
-        StructuredContentObject::Tbody(_) => {
-            println!("DEBUG[1410]: this is StructuredContentTbody ");
-        }
-        StructuredContentObject::Tfoot(_) => {
-            println!("DEBUG[1410]: this is StructuredContentTfoot ");
-        }
-        StructuredContentObject::Tr(_) => {
-            println!("DEBUG[1410]: this is StructuredContentTr ");
-        }
-        StructuredContentObject::Td(_) => {
-            println!("DEBUG[1410]: this is StructuredContentTd ");
-        }
-        StructuredContentObject::Th(_) => {
-            println!("DEBUG[1410]: this is StructuredContentTh ");
-        }
-        StructuredContentObject::Span(fields) => {
-            println!("DEBUG[1410]: this is StructuredContentSpan ");
-            if let Some(content) = fields.content.as_ref() {
-                test_structured_content(content)
-            }
-        }
-        StructuredContentObject::Div(fields) => {
-            if let Some(content) = fields.content.as_ref() {
-                test_structured_content(content)
-            }
-            println!("DEBUG[1410]: this is StructuredContentDiv");
-        }
-        StructuredContentObject::Ol(_) => {
-            println!("DEBUG[1410]: this is StructuredContentOl ");
-        }
-        StructuredContentObject::Ul(_) => {
-            println!("DEBUG[1410]: this is StructuredContentUl ");
-        }
-        StructuredContentObject::Li(_) => {
-            println!("DEBUG[1410]: this is StructuredContentLi ");
-        }
-        StructuredContentObject::Details(_) => {
-            println!("DEBUG[1410]: this is StructuredContentDetails ");
-        }
-        StructuredContentObject::Summary(_) => {
-            println!("DEBUG[1410]: this is StructuredContentSummary ");
-        }
-        StructuredContentObject::Img(_) => {
-            println!("DEBUG[1410]: this is StructuredContentImg ");
-        }
-        StructuredContentObject::A(_) => {
-            println!("DEBUG[1410]: this is StructuredContentA ");
-        }
-    }
-}
+mod r#impl;
 
 #[cfg(test)]
 mod test;
