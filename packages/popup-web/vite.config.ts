401f8879f4d2628a1d46edff515e3e4da1a70649
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-01-29 19:18:37 +0700

    build: config build

--- packages/popup-web/vite.config.ts
+++ packages/popup-web/vite.config.ts
@@ -1,8 +1,28 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
   root: "./src/sample-page/sample2",
+  build: {
+    lib: {
+      entry: path.resolve(import.meta.dirname, "src/main.tsx"),
+      name: "hanayomi",
+      fileName: "hanayomi",
+      formats: ["es"],
+    },
+    copyPublicDir: false,
+    cssCodeSplit: false,
+    cssMinify: false,
+    minify: false,
+    rolldownOptions: {
+      output: {
+        minify: false,
+      },
+    },
+    outDir: "../../../dist",
+    emptyOutDir: true,
+  },
 });
